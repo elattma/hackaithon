da6fdbbd03667250217ec4a790c3c925b7bc0c60
Mohamed Elattma <mo@mimo.team>
2023-05-16 21:14:04 -0700

    fix: add nextAction in response

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,3 +1,4 @@
+import { ActionType } from "@/orchestrator/model";
 import { traverseState } from "@/orchestrator/stateMachine";
 import { NextResponse } from "next/server";
 import { Configuration, OpenAIApi } from "openai";
@@ -11,5 +12,5 @@ export async function POST(request: Request) {
   const body = await request.json();
   const { state, action } = body;
   const newState = await traverseState(openai, state, action);
-  return NextResponse.json({ state: newState });
+  return NextResponse.json({ state: newState, nextAction: ActionType });
 }
