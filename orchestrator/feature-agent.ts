8b4d45eb98ddb85e8470bd8a48f15c128d5d6e56
Mohamed Elattma <mo@mimo.team>
2023-05-17 09:49:23 -0700

    feat: add some pseudocode

--- orchestrator/feature-agent.ts
+++ orchestrator/feature-agent.ts
@@ -3,6 +3,12 @@ import { State } from "@/orchestrator/model";
 
 export class FeatureAgent extends Agent {
   async act(state: State): Promise<State> {
+    // if state already has everything besides the features and the tables
+    // then just create the features and tables early return
+
+    // else if state has not yet identified problems
+    // identify problems and early return
+
     return Promise.resolve(state);
   }
 }
